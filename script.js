41df9a2d2e323197246d64bbda7812c5f9d03778
Ronier Lima <ronier.lim@gmail.com>
2024-02-12 22:17:49 -0300

    update

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "https://files.ronierlima.dev/api/profile/ronierlima-light.png")
   } else {
     // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "https://files.ronierlima.dev/api/profile/ronierlima.png")
   }
 }
