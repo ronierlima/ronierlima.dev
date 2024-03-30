8b6843a44c5c53ae5e6ba9e8e6070c85a68e1cb4
Ronier Lima <ronier.lim@gmail.com>
2024-03-30 12:08:29 -0300

    refactor: change light perfil

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "https://ronierlima.github.io/atlas/profile/ronierlima.png")
+    img.setAttribute("src", "https://ronierlima.github.io/atlas/profile/perfil.jpg")
   } else {
     // set tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "https://ronierlima.github.io/atlas/profile/ronierlima.png")
